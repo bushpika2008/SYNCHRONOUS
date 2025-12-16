89d6fa15ee2fdea0b6259410e2239a88d4eb2aa2
bushpika2008 <bushpika2008@gmail.com>
2025-12-16 11:04:20 +0530

    Add files via upload

---
+++ SYNCHRONOUS.v
@@ -0,0 +1,14 @@
+module SYNCHRONOUS (
+    input  wire clk,      
+    input  wire rst,      
+    output reg  [2:0] q   
+);
+
+always @(posedge clk) begin
+    if (rst)
+        q <= 3'b000;     
+    else
+        q <= q + 1;       
+end
+
+endmodule
